3f0f1daa46a04bf18ea327d0389b275639220dcf
Felix Fernandez <fernandezfelix460.gmail.com>
2019-10-22 13:59:06 -0400

    update

--- TriviaGame/assets/app.js
+++ TriviaGame/assets/app.js
@@ -1,49 +1,81 @@
 
-// $(document).ready(function () {
-function onSubmit() {
-  var score = 0;
+$(document).ready(function () {
+
+  $("#form").hide();
+  $("#timer").hide();
+
+  $("#start-button").click(function () {
+    $("#form").show();
+    $("#start-button").hide();
+    $("#timer").show();
+
+
+
+  });
+
+
+  var correctAns = 0;
   var numOfQuestions = 8;
-  var ansArr = ["b", "b", "d", "d", "c", "d"];
-  var q1 = document.forms["quiz"]["q1"].value;
-  var q2 = document.forms["quiz"]["q2"].value;
-  var q3 = document.forms["quiz"]["q3"].value;
-  var q4 = document.forms["quiz"]["q4"].value;
-  var q5 = document.forms["quiz"]["q5"].value;
-  var q6 = document.forms["quiz"]["q6"].value;
-  var q7 = document.forms["quiz"]["q7"].value;
-  var q8 = document.forms["quiz"]["q8"].value;
+  var wrongAns = 0;
 
-  for (var i = 1; i <= numOfQuestions; i++) {
-    if (eval("q" + i) === "") {
-      alert("you missed question number " + i);
+
+  var counter = 45;
+  var interval = setInterval(function () {
+   $("#timer").html(counter);
+    counter--;
+
+    if (counter < 0) {
+
+      clearInterval(interval);
     }
-  }
-  for (var i = 1; i <= numOfQuestions; i++);
-  if (eval("q" + i) === ansArr[i - 1]) {
-    score++;
-  }
-  var results = document.getElementById("results");
-  results.innerHTML = "<h2>You Scored " + score + " points out of " + numOfQuestions + "</h2>"
-  return false;
-}
+  }, 1000);
+
+  $("#submit").click(function () {
+
+
+    var ansArr = ["b", "b", "d", "d", "c", "d"];
+    var questions = ["q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8"];
+    var q1 = document.forms["quiz"]["q1"].value;
+    var q2 = document.forms["quiz"]["q2"].value;
+    var q3 = document.forms["quiz"]["q3"].value;
+    var q4 = document.forms["quiz"]["q4"].value;
+    var q5 = document.forms["quiz"]["q5"].value;
+    var q6 = document.forms["quiz"]["q6"].value;
+    var q7 = document.forms["quiz"]["q7"].value;
+    var q8 = document.forms["quiz"]["q8"].value;
+    var userInput = $(this).attr("value");
+
+
+    for (var i = 1; i < questions.length; i++) {
+      if (userInput === ansArr[i]) {
+        correctAns++;
+
+
+      } else {
+        wrongAns++;
+      }
+    }
+
+    var results = $("#results");
+    results.html("You Scored " + score + " points out of " + numOfQuestions + "</h2>")
+   
+
+  });
+
+});
 
 
 
-    // $(".form").hide();
-    // $("#timer").hide();
 
     // // var stopTimer;
     // // var intervalId;
-    // var counter = 45;
+
     // // var correct = 0;
     // // var incorrect = 0;
     // // var unaswered = 0;
     // // var clockRunnig = false;
 
-    // $("#start-button").click(function () {
-    //     $(".form").show();
-    //     $("#start-button").hide();
-    //     $("#timer").show();
+    // 
     //     $(".results").hide();
 
     //     $("#submit").click( function(){
@@ -53,18 +85,7 @@ function onSubmit() {
 
     //   });
 
-    //     intervalId = setInterval(counter, 1000);
-    //     setInterval(function () {
-    //         counter--;
-    //         // console.log(counter);
-    //         $("#timer").html("Time remaining " + "00: " + counter);
 
-    //         if (counter === 0) {
-    //             clearInterval(counter);
-    //         }
-
-
-    //     }, 1000);
 
     // });
 
